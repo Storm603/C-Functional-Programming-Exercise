a96f1d1a6bb0a6ac780ee30a97f191d2203b2bb2
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-02-06 16:37:08 +0200

    Add files via upload

---
+++ T10. Party Reservation Filter Module/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T10._Party_Reservation_Filter_Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Predicate<string>> predicates = new Dictionary<string, Predicate<string>>();
+                List<string> names = Console.ReadLine().Split().ToList();
+
+            string action = Console.ReadLine();
+
+            while (action != "Print")
+            {
+                string[] split = action.Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                
+                if (split[0] == "Add filter")
+                {
+                    predicates.Add(split[1] + split[2], AddFilter(split[1], split[2]));
+                }
+                else if (split[0] == "Remove filter")
+                {
+                    predicates.Remove(split[1] + split[2]);
+                }
+                action = Console.ReadLine();
+            }
+
+            List<string> leftovers = new List<string>();
+
+            foreach (var (key, value) in predicates)
+            {
+                names.RemoveAll(value);
+            }
+
+            Console.WriteLine(string.Join(" ", names));
+        }
+
+        //public List<Predicate<string>> RemoveFilter(List<Predicate<string>>, )
+        //{
+
+        //}
+
+        public static Predicate<string> AddFilter(string action ,string value)
+        {
+
+            if (action == "Starts with")
+            {
+                return x => x.StartsWith(value);
+            }
+
+            if (action == "Ends with")
+            {
+                return x => x.EndsWith(value);
+            }
+
+            if (action == "Length")
+            {
+                return x => x.Length == int.Parse(value);
+            }
+
+            return x => x.Contains(value);
+            
+        }
+    }
+}
---
+++ T11. TriFunction/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace T11._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split();
+
+
+            //Func<string[], int, string> filtering = (x, y) =>
+            //{
+            //    foreach (string name in x)
+            //    {
+            //        if (y(name))
+            //        {
+                        
+            //        }
+            //    }
+            //};
+            
+
+            Func<string, int, bool> countingChars = (x, y) =>
+            {
+                int sumOfChars = 0;
+
+                foreach (char letter in x)
+                {
+                    sumOfChars += letter;
+                }
+
+                if (sumOfChars >= y)
+                {
+                    return true;
+                }
+
+                return false;
+            };
+
+            Func<string[], Func<string, int, bool>, string> filter = (name, sum) =>
+            {
+
+                foreach (string names in name)
+                {
+                    if (sum(names, num))
+                    {
+                        return names;
+                    }
+                }
+
+                return null;
+            };
+            Console.WriteLine(filter(names, countingChars));
+            
+        }
+    }
+}
